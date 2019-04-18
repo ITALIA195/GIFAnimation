8f36fdcd7d15e2dc12e501b3066c562373683455
mHawk <italia195@outlook.it>
2019-04-18 20:20:54 +0200

    [WIP] Creating multiple cubes

--- RecreatingGIF/Graphics/Objects/Rectangle.cs
+++ RecreatingGIF/Graphics/Objects/Rectangle.cs
@@ -13,7 +13,7 @@ namespace RecreatingGIF.Graphics.Objects
             _shader = shader;
             _buffers = new Buffers(Buffer.VertexBuffer | Buffer.VertexArray);
             
-            BindData();
+//            BindData();
         }
 
         private void BindData()
@@ -24,7 +24,11 @@ namespace RecreatingGIF.Graphics.Objects
 
             var vertices = new[]
             {
-                0, 0, 0
+//                1, 1, 1,
+//                20, 20, 20,
+//                5, 5, 5,
+//                2, 2, 2,
+                1, 1, 16,
             };
             
             // ==================
@@ -39,9 +43,9 @@ namespace RecreatingGIF.Graphics.Objects
             // == Create VAO ==
             // ================
             
-            GL.BindVertexArray(_buffers.VertexArray);
-            
             _shader.Use();
+            
+            GL.BindVertexArray(_buffers.VertexArray);
             GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
             GL.EnableVertexAttribArray(0);
             
@@ -54,12 +58,11 @@ namespace RecreatingGIF.Graphics.Objects
             _shader.Use();
 
             anim += 0.05f;
-            
             var projection = Matrix4.Identity;
             var view = Matrix4.Identity;
             var model = Matrix4.Identity;
 
-            const float scale = 0.05f;
+            const float scale = 0.1f;
             model *= Matrix4.CreateScale(scale, scale, scale);
 
             view *= Matrix4.CreateScale(1f / Window.AspectRatio, 1, 1);
@@ -69,13 +72,26 @@ namespace RecreatingGIF.Graphics.Objects
 //            projection *= Matrix4.CreatePerspectiveFieldOfView(MathHelper.PiOver2, Window.AspectRatio, 1f, 50f);
 
             _shader.SetUniformValue("animation", anim);
-            _shader.SetUniformValue("centerOfAnimation", new Vector3(0, 0, 0));
+//            _shader.SetUniformValue("centerOfAnimation", new Vector3(0, 0, 0));
             _shader.SetUniformValue("projection", ref projection);
             _shader.SetUniformValue("view", ref view);
             _shader.SetUniformValue("model", ref model);
             
+            
+            var vertices = new[]
+            {
+                1, 1f, 1f,
+            };
+            
+            GL.BindBuffer(BufferTarget.ArrayBuffer, _buffers.VertexBuffer);
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+            
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+            
             GL.BindVertexArray(_buffers.VertexArray);
-            GL.DrawArrays(PrimitiveType.Points, 0, 3);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, _buffers.VertexBuffer);
+            GL.DrawArrays(PrimitiveType.Points, 0, 5000);
         }
     }
 }
