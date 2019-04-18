6b86a21f4ab7bc151d4829cc80b9c348725116cd
mHawk <italia195@outlook.it>
2019-04-18 21:29:07 +0200

    Added multiple cubes (Finishes 8f36fdcd7d15e2dc12e501b3066c562373683455)

--- RecreatingGIF/Graphics/Objects/Rectangle.cs
+++ RecreatingGIF/Graphics/Objects/Rectangle.cs
@@ -5,15 +5,18 @@ namespace RecreatingGIF.Graphics.Objects
 {
     public class Rectangle
     {
+        private const int NumberOfBoxes = 15;
         private readonly Shader _shader;
         private readonly Buffers _buffers;
+
+        private readonly float[] _vertices = new float[3 * NumberOfBoxes * NumberOfBoxes];
         
         public Rectangle(Shader shader)
         {
             _shader = shader;
             _buffers = new Buffers(Buffer.VertexBuffer | Buffer.VertexArray);
             
-//            BindData();
+            BindData();
         }
 
         private void BindData()
@@ -22,14 +25,17 @@ namespace RecreatingGIF.Graphics.Objects
             // == Create Data ==
             // =================
 
-            var vertices = new[]
+            int i = 0;
+            for (float x = -NumberOfBoxes / 2f; x < NumberOfBoxes / 2f; x++)
             {
-//                1, 1, 1,
-//                20, 20, 20,
-//                5, 5, 5,
-//                2, 2, 2,
-                1, 1, 16,
-            };
+                for (float y = -NumberOfBoxes / 2f; y < NumberOfBoxes / 2f; y++)
+                {
+                    _vertices[i + 0] = x * 2;
+                    _vertices[i + 1] = 0;
+                    _vertices[i + 2] = y * 2;
+                    i += 3;
+                }
+            }
             
             // ==================
             // == Bind Buffers ==
@@ -37,7 +43,7 @@ namespace RecreatingGIF.Graphics.Objects
             
             // VBO (Vertices)
             GL.BindBuffer(BufferTarget.ArrayBuffer, _buffers.VertexBuffer);
-            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+            GL.BufferData(BufferTarget.ArrayBuffer, _vertices.Length * sizeof(float), _vertices, BufferUsageHint.StaticDraw);
             
             // ================
             // == Create VAO ==
@@ -51,18 +57,18 @@ namespace RecreatingGIF.Graphics.Objects
             
             GL.BindBuffer(BufferTarget.ArrayBuffer, _buffers.VertexBuffer);
         }
-
+        
         private float anim;
         public void Draw()
         {
             _shader.Use();
 
-            anim += 0.05f;
+            anim += 0.03f;
             var projection = Matrix4.Identity;
             var view = Matrix4.Identity;
             var model = Matrix4.Identity;
 
-            const float scale = 0.1f;
+            const float scale = 0.04f;
             model *= Matrix4.CreateScale(scale, scale, scale);
 
             view *= Matrix4.CreateScale(1f / Window.AspectRatio, 1, 1);
@@ -72,26 +78,14 @@ namespace RecreatingGIF.Graphics.Objects
 //            projection *= Matrix4.CreatePerspectiveFieldOfView(MathHelper.PiOver2, Window.AspectRatio, 1f, 50f);
 
             _shader.SetUniformValue("animation", anim);
-//            _shader.SetUniformValue("centerOfAnimation", new Vector3(0, 0, 0));
+            _shader.SetUniformValue("centerOfAnimation", new Vector3(0, 0, 0));
             _shader.SetUniformValue("projection", ref projection);
             _shader.SetUniformValue("view", ref view);
             _shader.SetUniformValue("model", ref model);
             
             
-            var vertices = new[]
-            {
-                1, 1f, 1f,
-            };
-            
-            GL.BindBuffer(BufferTarget.ArrayBuffer, _buffers.VertexBuffer);
-            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
-            
-            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
-            GL.EnableVertexAttribArray(0);
-            
             GL.BindVertexArray(_buffers.VertexArray);
-            GL.BindBuffer(BufferTarget.ArrayBuffer, _buffers.VertexBuffer);
-            GL.DrawArrays(PrimitiveType.Points, 0, 5000);
+            GL.DrawArrays(PrimitiveType.Points, 0, NumberOfBoxes * NumberOfBoxes);
         }
     }
 }
