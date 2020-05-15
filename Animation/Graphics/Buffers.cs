3051fb23f09cea1f703fe63245f1f7cd90809aff
Hawk <italia195@outlook.it>
2020-05-15 19:30:37 +0200

    Added error check on Buffers

--- Animation/Graphics/Buffers.cs
+++ Animation/Graphics/Buffers.cs
@@ -5,31 +5,75 @@ namespace Animation.Graphics
 {
     public class Buffers
     {
-        private readonly int _vertexBuffer;
-        private readonly int _elementBuffer;
-        private readonly int _colorBuffer;
-        private readonly int _textureBuffer;
-        private readonly int _vertexArray;
+        private readonly int _vertexBuffer = -1;
+        private readonly int _elementBuffer = -1;
+        private readonly int _colorBuffer = -1;
+        private readonly int _textureBuffer = -1;
+        private readonly int _vertexArray = -1;
 
         public Buffers(Buffer buffer)
         {
-            if ((buffer & Buffer.VBO) != 0)
+            if ((buffer & Buffer.VertexBuffer) != 0)
                 _vertexBuffer = GL.GenBuffer();
-            if ((buffer & Buffer.EBO) != 0)
+            if ((buffer & Buffer.ElementBuffer) != 0)
                 _elementBuffer = GL.GenBuffer();
-            if ((buffer & Buffer.CBO) != 0)
+            if ((buffer & Buffer.ColorBuffer) != 0)
                 _colorBuffer = GL.GenBuffer();
-            if ((buffer & Buffer.TBO) != 0)
+            if ((buffer & Buffer.TextureBuffer) != 0)
                 _textureBuffer = GL.GenBuffer();
-            if ((buffer & Buffer.VAO) != 0)
+            if ((buffer & Buffer.VertexArray) != 0)
                 _vertexArray = GL.GenVertexArray();
         }
-        
-        public int VertexBuffer => _vertexBuffer;
-        public int ColorBuffer => _colorBuffer;
-        public int ElementBuffer => _elementBuffer;
-        public int TextureBuffer => _textureBuffer;
-        public int VertexArray => _vertexArray;
+
+        public int VertexBuffer
+        {
+            get
+            {
+                if (_vertexBuffer == -1)
+                    throw new Exception("Accessing nonexistent buffer");
+                return _vertexBuffer;
+            }
+        }
+
+        public int ColorBuffer
+        {
+            get
+            {
+                if (_colorBuffer == -1)
+                    throw new Exception("Accessing nonexistent buffer");
+                return _colorBuffer;
+            }
+        }
+
+        public int ElementBuffer
+        {
+            get
+            {
+                if (_elementBuffer == -1)
+                    throw new Exception("Accessing nonexistent buffer");
+                return _elementBuffer;
+            }
+        }
+
+        public int TextureBuffer
+        {
+            get
+            {
+                if (_textureBuffer == -1)
+                    throw new Exception("Accessing nonexistent buffer");
+                return _textureBuffer;
+            }
+        }
+
+        public int VertexArray
+        {
+            get
+            {
+                if (_vertexArray == -1)
+                    throw new Exception("Accessing nonexistent buffer");
+                return _vertexArray;
+            }
+        }
     }
 
     [Flags]
